848d22e22e03eb00fe141efd707da6e7658934fa
Jsamuel1919 <156345975+Jsamuel1919@users.noreply.github.com>
2024-03-11 19:56:51 +0000

    agrego img y mg

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import PropTypes from "prop-types"
 const Card = (props) => {
     return(
-        <div class="card" style= {{width: "18rem", display: "flex"}}>
-        <img src="..." class="card-img-top" alt="..."/>
+        <div class="card m-3" style= {{width: "18rem", display: "flex",}}>
+        <img src="https://images.pexels.com/photos/7966748/pexels-photo-7966748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -13,3 +13,4 @@ const Card = (props) => {
     )
 }
 export default Card;
+
